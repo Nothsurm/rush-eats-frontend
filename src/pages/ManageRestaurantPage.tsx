892ee89713ac384fba173ca89d04d0f1df52ab18
Mike Rushton <mrush94ton@gmail.com>
2024-03-27 20:25:23 +0000

    created restaurant api request

--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,6 +1,9 @@
+import { useCreateMyRestaurant } from "@/api/MyRestaurantApi";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm.tsx";
 
 
 export default function ManageRestaurantPage() {
-  return <ManageRestaurantForm />
+  const { createRestaurant, isLoading } = useCreateMyRestaurant()
+
+  return <ManageRestaurantForm onSave={createRestaurant} isLoading={isLoading} />
 }
