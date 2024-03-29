8b2f9930b46acc3e54e5dd7e5e43212df24f2d96
Mike Rushton <mrush94ton@gmail.com>
2024-03-29 17:10:00 +0000

    created the searchbar component

---
+++ src/pages/SearchPage.tsx
@@ -0,0 +1,10 @@
+import { useParams } from "react-router-dom"
+
+
+export default function SearchPage() {
+    const { city } = useParams()
+    
+  return (
+    <div>User searched for {city} </div>
+  )
+}
