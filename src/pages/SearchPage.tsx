502ddcb754faf248ec3e84f09dfa92240f81d9f3
Mike Rushton <mrush94ton@gmail.com>
2024-03-29 18:07:06 +0000

    created search api request

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,10 +1,12 @@
+import { useSearchRestaurants } from "@/api/RestaurantApi";
 import { useParams } from "react-router-dom"
 
 
 export default function SearchPage() {
-    const { city } = useParams()
+    const { city } = useParams();
+    const { results } = useSearchRestaurants(city)
     
   return (
-    <div>User searched for {city} </div>
+    <div>User searched for {city} <span>{results?.data.map((restaurant) => <span>found - {restaurant.restaurantName}, {restaurant.city}</span>)}</span> </div>
   )
 }
