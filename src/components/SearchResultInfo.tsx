d9b6ddefc2e7f4f8b467cc41ea7cf0aea49a4adf
Mike Rushton <mrush94ton@gmail.com>
2024-03-31 12:45:34 +0100

    created pagination component and functionality

--- src/components/SearchResultInfo.tsx
+++ src/components/SearchResultInfo.tsx
@@ -9,7 +9,7 @@ const SearchResultInfo = ({total, city}: Props) => {
     return (
         <div className="text-cl font-bold flex flex-col gap-3 justify-between lg:items-center lg:flex-row">
             <span>
-                {total} Resuarants found in {city}
+                {total} Restuarants found in {city.toUpperCase()}
                 <Link to='/' className="ml-1 text-sm font-semibold underline cursor-pointer text-blue-500">
                     Change Location
                 </Link>
