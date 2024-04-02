47355f0c554cef909756bdacc2a7ba9cf310793c
Mike Rushton <mrush94ton@gmail.com>
2024-04-02 14:13:14 +0100

    created modal popup on delivery details

--- src/components/CheckoutButton.tsx
+++ src/components/CheckoutButton.tsx
@@ -2,12 +2,22 @@ import { useAuth0 } from "@auth0/auth0-react"
 import { useLocation } from "react-router-dom"
 import { Button } from "./ui/button"
 import LoadingButton from "./LoadingButton"
+import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog"
+import { UserFormData, UserProfileForm } from "@/forms/user-profile-form/UserProfileForm"
+import { useGetMyUser } from "@/api/MyUserApi"
+
+type Props = {
+    onCheckout: (userFormData: UserFormData) => void;
+    disabled: boolean;
+}
 
 
-export default function CheckoutButton() {
+export default function CheckoutButton({ onCheckout, disabled}: Props) {
     const { isAuthenticated, isLoading: isAuthLoading, loginWithRedirect } = useAuth0()
 
     const { pathname } = useLocation()
+
+    const { currentUser, isLoading: isGetUserLoading } = useGetMyUser()
 
     const onLogin = async () => {
         await loginWithRedirect({
@@ -25,7 +35,20 @@ export default function CheckoutButton() {
         )
     }
 
-    if (isAuthLoading) {
+    if (isAuthLoading || !currentUser) {
         return <LoadingButton />
     }
+
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button className="flex-1 bg-orange-500" disabled={disabled}>
+                    Go to checkout
+                </Button>
+            </DialogTrigger>
+            <DialogContent className="max-w-[452px] md:min-w-[700px] bg-gray-50">
+                <UserProfileForm currentUser={currentUser} onSave={onCheckout} isLoading={isGetUserLoading} />
+            </DialogContent>
+        </Dialog>
+    )
 }
