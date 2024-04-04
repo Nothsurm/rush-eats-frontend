f093acc042d10365d6e2684141268a0d2bb5b654
Mike Rushton <mrush94ton@gmail.com>
2024-04-04 16:24:28 +0100

    fixed order status route bug

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -9,7 +9,7 @@ export default function MainNav() {
     <span className="flex space-x-2 items-center">
       {isAuthenticated ? (
         <>
-          <Link to='order-status' className="font-bold hover:text-orange-500">
+          <Link to='/order-status' className="font-bold hover:text-orange-500">
             Order Status
           </Link>
           <UsernameMenu />
