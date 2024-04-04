41ecde883b890a248a8f562435758bd938d020a4
Mike Rushton <mrush94ton@gmail.com>
2024-04-04 10:52:15 +0100

    created order status page

---
+++ src/components/OrderStatusHeader.tsx
@@ -0,0 +1,34 @@
+import { Order } from "@/types"
+
+type Props = {
+    order: Order
+}
+
+const OrderStatusHeader = ({ order }: Props) => {
+
+    const getExpectedDelivery = () => {
+        const created = new Date(order.createdAt)
+
+        created.setMinutes(
+            created.getMinutes() + order.restaurant.estimatedDeliveryTime
+        );
+
+        const hours = created.getHours();
+        const minutes = created.getMinutes();
+
+        // convert 12:2 to 12:02
+        const paddedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+
+        return `${hours}:${paddedMinutes}`;
+    }
+    return (
+        <>
+            <h1 className="text-4xl font-bold tracking-tighter flex flex-col gap-5 md:flex-row md:justify-between">
+                <span>Order Status: {order.status}</span>
+                <span>Expected by: {getExpectedDelivery()}</span>
+            </h1>
+        </>
+    )
+}
+
+export default OrderStatusHeader;
