1711d1805aa4022cebb4e96802a1bb15072a39cf
Mike Rushton <mrush94ton@gmail.com>
2024-04-04 10:32:24 +0100

    get customer orders request

--- src/types.ts
+++ src/types.ts
@@ -34,4 +34,32 @@ export type RestaurantSearchResponse = {
         page: number;
         pages: number;
     }
+};
+
+export type OrderStatus = 
+    "placed"| 
+    "paid"| 
+    "inProgress"| 
+    "outForDelivery"| 
+    "delivered"
+
+export type Order = {
+    _id: string;
+    restaurant: Restaurant;
+    user: User;
+    cartItems: {
+        menuItemId: string;
+        name: string;
+        quantity: string;
+    }[];
+    deliveryDetails: {
+        name: string;
+        addressLine1: string;
+        city: string;
+        email: string;
+    };
+    totalAmount: number;
+    status: OrderStatus;
+    createdAt: string;
+    restaurantId: string
 }
